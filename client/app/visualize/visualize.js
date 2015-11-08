3c94c663ba72403c4f63150ef2ef492c27e6f577
mega0022 <mega0022@morris.umn.edu>
2015-11-07 21:15:01 -0600

    Fixed build file and created new view to see schemas before viewing the instances

--- client/app/visualize/visualize.js
+++ client/app/visualize/visualize.js
@@ -3,8 +3,13 @@
 angular.module('gAmPieApp')
   .config(function ($stateProvider) {
     $stateProvider
-      .state('visualize', {
+      .state('visualizeInstance', {
         url: '/visualize/:visCategory',
+        templateUrl: 'app/visualize/visualize.html',
+        controller: 'VisualizeCtrl'
+      })
+      .state('visualize', {
+        url: '/visualize',
         templateUrl: 'app/visualize/visualize.html',
         controller: 'VisualizeCtrl'
       });
