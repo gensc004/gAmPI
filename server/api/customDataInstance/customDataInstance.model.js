1f7499023cb0b3c307adac12b0965908d9baf166
gensc004 <gensc004@morris.umn.edu>
2015-11-08 00:16:45 -0600

    Made a check for congruence between customDataInstance and customDataSchema

--- server/api/customDataInstance/customDataInstance.model.js
+++ server/api/customDataInstance/customDataInstance.model.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 var ObjectIdSchema = Schema.ObjectId;
 var ObjectId = mongoose.Types.ObjectId;
+var CustomDataSchema = require('../customDataSchema/customDataSchema.model');
 
 var CustomDataInstanceSchema = new Schema({
   schemaId: ObjectIdSchema,
@@ -19,5 +20,28 @@ CustomDataInstanceSchema.statics.query = function query(q) {
 
   return this.find(query);
 }
+
+CustomDataInstanceSchema.pre('save', function(next) {
+	var self = this
+	var query = {
+		schemaId: this.schemaId
+	}
+	CustomDataSchema.findOne(query).exec(function(err, schema) {
+		console.log(schema);
+		var conformsToSchema = true;
+		for(var i = 0; i < schema.dataSchema.length; i++) {
+			if(!self.dataPoints[0].hasOwnProperty(schema.dataSchema[i].field)) {
+				conformsToSchema = false;
+			}
+		}
+		if(conformsToSchema) {
+			next();
+		} else {
+			next(new Error("Improper dataPoints for schema"));
+		}
+		
+	})
+	
+})
 
 module.exports = mongoose.model('CustomDataInstance', CustomDataInstanceSchema);
